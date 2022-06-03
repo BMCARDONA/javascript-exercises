7145c7baf4c55e9edf7e411e6896bdd9d8e0e51e
Bradley Cardona <bradleymathematics@gmail.com>
2022-06-03 12:36:57 -0400

    Complete Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
 
+// arguments object: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+   
+// The arguments object is not an Array. It is similar, but lacks all Array properties except LENGTH. //
+
+// helpful link: https://stackoverflow.com/questions/9882284/looping-through-array-and-removing-items-without-breaking-for-loop
+
+
+const removeFromArray = function(myArray, ...valuesToCheck) {
+  for (let i = myArray.length - 1; i >= 0; --i) {
+    for (let value of valuesToCheck) {
+      if (myArray[i] === value) {
+        // splice(index, # of items)
+        myArray.splice(i, 1);
+      }
+    }
+  }
+  return myArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
