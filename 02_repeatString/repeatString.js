c98b7f53eff068274451b1022b0f0ce7edd6268e
Bradley Cardona <bradleymathematics@gmail.com>
2022-06-03 10:30:45 -0400

    Complete Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return("ERROR");
+    } else {
+          // Cannot simply write <let originalString>, since it'll have an undefined value
+        let originalString = "";
+        for (let i = 0; i < num; ++i) {
+            originalString += string;
+        }
+        return(originalString);
+    }
 };
+
+repeatString("hey", 3);
+
 
 // Do not edit below this line
 module.exports = repeatString;
