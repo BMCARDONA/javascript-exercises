9676e6494bf0160521247c14e9c17bda910b4391
Bradley Cardona <bradleymathematics@gmail.com>
2022-06-03 10:48:59 -0400

    Complete Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,23 @@
-const reverseString = function() {
 
+/*
+======= Wrong Answer =======
+const reverseString = function(string) {
+    let originalString = string;
+    let newString = "";
+    for (let i = 0; i < originalString.length; ++i) {
+      newString = originalString.unshift(originalString[i]);
+    }
+    return newString;
+};
+*/
+
+const reverseString = function(string) {
+  let originalString = string;
+  let newString = "";
+  for (let i = originalString.length - 1; i >= 0; --i) {
+    newString += originalString[i];
+  }
+  return newString;
 };
 
 // Do not edit below this line
