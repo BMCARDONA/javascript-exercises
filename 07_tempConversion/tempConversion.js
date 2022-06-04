3fd64478ca75f134c7f991be6dfb7ea9ccaf498a
Bradley Cardona <bradleymathematics@gmail.com>
2022-06-03 23:39:42 -0400

    Complete Exericse 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fNumber) {
+    let celsius = (fNumber - 32) * (5/9);
+    let roundedCelsius = Math.round(celsius * 10) / 10;
+    return(roundedCelsius);
 };
 
-const ctof = function() {
-
+const ctof = function(cNumber) {
+    let fahrenheit = cNumber * (9/5) + 32;
+    let roundedFahrenheit = Math.round(fahrenheit * 10) / 10;
+    return (roundedFahrenheit);
 };
 
 // Do not edit below this line
