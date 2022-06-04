43b184d4bbaa9f955c719c82e1154353b2a2c2e3
Bradley Cardona <bradleymathematics@gmail.com>
2022-06-03 23:15:11 -0400

    Complete Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
 
+
+const sumAll = function(firstNumber, secondNumber) {
+    // If an operand is not a number, return "ERROR"
+    if (typeof(firstNumber) != "number" || typeof(secondNumber) != "number") {
+      return("ERROR");
+    }
+    // if an operand holds a negative value, return "ERROR"
+    else if (firstNumber < 0 || secondNumber < 0) {
+      return("ERROR");
+    }
+    else {
+      let sum = 0;
+      if (firstNumber < secondNumber) {
+        for (let i = firstNumber; i <= secondNumber; ++i) {
+          sum += i;
+        }
+        return sum;
+      } else if (firstNumber > secondNumber) {
+        for (let i = firstNumber; i >= secondNumber; --i) {
+          sum += i;
+        }
+        return sum;
+      }
+    }
 };
 
 // Do not edit below this line
